74deeb9d8afafd4b8f6dc1f46e519ac2d97130ac
iSayU <103714465+nguyenvinhhuy123@users.noreply.github.com>
2024-01-01 19:44:23 +0700

    feat: initialize DeadState in state machine

---
+++ Assets/Scenes/Script/MainCharacterMovement/State/DamagedState/DeadState.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class DeadState : BaseState
+{
+    private float _deadStateDurationTime; 
+    public DeadState(MainCharacterMovementStateMachine _machine) : base(_machine)
+    {
+        ANIMATION_PARAM = "PlayerDead";
+    }
+    public override void OnEnter()
+    {
+        _deadStateDurationTime = _machine._data.m_DeadStateDuration;
+        base.OnEnter();
+    }
+    public override void OnUpdate()
+    {
+        _deadStateDurationTime -= Time.deltaTime;
+    }
+    public override void OnExit()
+    {
+        //GameObject.Destroy(_machine._controller.gameObject);
+        base.OnExit();
+    }
+    public override void OnMovement()
+    {
+        //*let empty instead of calling base OnMovement by default
+    }
+    public override void SpriteFlip()
+    {
+        //*Keep empty instead of calling base */
+    }
+    public override void StateCondition()
+    {
+        if (_deadStateDurationTime > 0f) return;
+        OnExit();
+    }
+}
