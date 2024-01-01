d228cc3f5a35528b7a8cdad75c07ad0999480749
iSayU <103714465+nguyenvinhhuy123@users.noreply.github.com>
2024-01-01 16:44:33 +0700

    feat: implement StateCondition, OnEnter and OnExit for DamagedState

--- Assets/Scenes/Script/MainCharacterMovement/State/DamagedState/DamageState.cs
+++ Assets/Scenes/Script/MainCharacterMovement/State/DamagedState/DamageState.cs
@@ -10,6 +10,8 @@ public class DamageState : BaseState
     }
     public override void OnEnter()
     {
+        _machine._sharedData.StayInDamagedStateTime = _machine._data.m_DamagedStateDuration;
+        OnKnockBack();
         base.OnEnter();
     }   
     public override void OnExit()
@@ -34,9 +36,35 @@ public class DamageState : BaseState
         *Check facing direction
         *initial force = knockback x * facing dir , knockback y 
         */
+        float knockBackXDirection = -GetFacingDirection().x;
+        Vector2 targetKnockBackForce 
+        = new Vector2(
+            knockBackXDirection*_machine._data.m_knockBackForce.x, 
+            _machine._data.m_knockBackForce.y);
+        
+        _machine._reusableProperty.m_rigidBody2D.velocity = targetKnockBackForce;
     }
     public override void StateCondition()
     {
-        base.StateCondition();
+        if (_machine._sharedData.StayInDamagedStateTime > 0f) return;
+        if (!OnGround()) 
+        {
+            _machine.OnChangeState(_machine.Fall);
+            return;
+        }
+        if (_machine._sharedData.MovementInput != 0f)
+        {
+            _machine.OnChangeState(_machine.Run);
+        }
+        if (_machine._reusableProperty.m_rigidBody2D.velocity.x == 0f
+        && _machine._sharedData.MovementInput == 0)
+        {
+            _machine.OnChangeState(_machine.Idle);
+            return;
+        }
+    }
+    public override void SpriteFlip()
+    {
+        //*Keep empty instead of calling base */
     }
 }
