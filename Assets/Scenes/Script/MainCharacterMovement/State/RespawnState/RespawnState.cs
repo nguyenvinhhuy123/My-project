9bb0af50c548d79e68b0d88377ca3280df9b7449
iSayU <103714465+nguyenvinhhuy123@users.noreply.github.com>
2024-01-02 19:56:48 +0700

    feat: add Respawn State script

---
+++ Assets/Scenes/Script/MainCharacterMovement/State/RespawnState/RespawnState.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RespawnState : BaseState
+{
+    private float _reSpawnStateDurationTimer;
+    public RespawnState(MainCharacterMovementStateMachine _machine) : base(_machine)
+    {
+        ANIMATION_PARAM = "PlayerSpawn";
+    }
+    public override void OnEnter()
+    {
+        _reSpawnStateDurationTimer = _machine._data.m_reSpawnDuration;
+        base.OnEnter();
+    }
+    public override void OnUpdate()
+    {
+        _reSpawnStateDurationTimer -= Time.deltaTime;
+    }
+    public override void OnMovement()
+    {
+        //*let empty instead of calling base OnMovement by default
+    }
+    public override void SpriteFlip()
+    {
+        //*Keep empty instead of calling base */
+    }
+    public override void StateCondition()
+    {
+        if (_reSpawnStateDurationTimer > 0f) return;
+        _machine.OnChangeState(_machine.Idle);
+    }
+}
