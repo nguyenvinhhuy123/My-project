3de62ed3e5ba5fc74c23ce89c1a5acced8d0f915
iSayU <103714465+nguyenvinhhuy123@users.noreply.github.com>
2024-01-02 10:10:47 +0700

    feat: GM imeplement OnCharDestroy and respawn coroutine

--- Assets/Scenes/Script/Manager/GameManager.cs
+++ Assets/Scenes/Script/Manager/GameManager.cs
@@ -5,16 +5,41 @@ using Utilities;
 
 public class GameManager : PersistenceSingleton<GameManager>
 {
-    private MainCharacterController m_character;
+    [SerializeField] private GameObject m_mainCharacterPrefab;
+    private MainCharacterController m_characterInstance;
+    public MainCharacterController CharacterInstance {get{ return m_characterInstance;}}
     [SerializeField] private int m_characterLife;
     public int CharacterLife {get {return m_characterLife;}}
-    [SerializeField] private int m_reSpawnWaitTime; 
-    public int ReSpawnWaitTime {get {return m_reSpawnWaitTime;}}
+    [SerializeField] private float m_reSpawnWaitTime; 
+    public float ReSpawnWaitTime {get {return m_reSpawnWaitTime;}}
+    private Vector3 _lastCheckPoint;
+    public Vector3 LastCheckPoint {get {return _lastCheckPoint;}}
     protected override void Awake() {
         base.Awake();
 
-        m_character = FindAnyObjectByType<MainCharacterController>()
-        .GetComponent<MainCharacterController>();
-
+        //!hard code checkpoint
+        _lastCheckPoint = new Vector3(-4f, 3f, 0f);
     }
+    private void Start() {
+        StartCoroutine(nameof(ReSpawnWaitRoutine));
+    }
+    /// <summary>
+    /// Callback for character onDestroy
+    /// </summary>
+    public void OnCharacterDestroy()
+    {
+        StartCoroutine(nameof(ReSpawnWaitRoutine));
+    }
+    private IEnumerator ReSpawnWaitRoutine()
+    {
+        Debug.Log("Coroutine");
+        yield return new WaitForSeconds(m_reSpawnWaitTime);
+        SpawnNewCharacterInstance();
+    }
+    private void SpawnNewCharacterInstance()
+    {
+        Instantiate(m_mainCharacterPrefab, _lastCheckPoint, Quaternion.identity)
+        .TryGetComponent<MainCharacterController>(out MainCharacterController m_characterInstance);
+    }
+
 }
