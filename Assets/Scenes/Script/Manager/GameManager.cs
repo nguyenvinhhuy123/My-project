7f7cb732b9d90e46752684fafc486527ad76a0ca
iSayU <103714465+nguyenvinhhuy123@users.noreply.github.com>
2024-01-05 10:13:20 +0700

    refactor: inject logic to other classes

--- Assets/Scenes/Script/Manager/GameManager.cs
+++ Assets/Scenes/Script/Manager/GameManager.cs
@@ -5,42 +5,9 @@ using Utilities;
 
 public class GameManager : PersistenceSingleton<GameManager>
 {
-    [SerializeField] private GameObject m_mainCharacterPrefab;
-    private MainCharacterController m_characterInstance;
-    public MainCharacterController CharacterInstance {get{ return m_characterInstance;}}
-    [SerializeField] private int m_characterLife;
-    public int CharacterLife {get {return m_characterLife;}}
-    [SerializeField] private float m_reSpawnWaitTime; 
-    public float ReSpawnWaitTime {get {return m_reSpawnWaitTime;}}
-    private Vector3 _lastCheckPoint;
-    public Vector3 LastCheckPoint {get {return _lastCheckPoint;}}
     protected override void Awake() {
         base.Awake();
-
-        //!hard code checkpoint
-        _lastCheckPoint = new Vector3(-4f, 3f, 0f);
     }
     private void Start() {
-        StartCoroutine(nameof(ReSpawnWaitRoutine));
     }
-    /// <summary>
-    /// Callback for character onDestroy
-    /// </summary>
-    public void OnCharacterDestroy()
-    {
-        StartCoroutine(nameof(ReSpawnWaitRoutine));
-    }
-    private IEnumerator ReSpawnWaitRoutine()
-    {
-        Debug.Log("Coroutine");
-        yield return new WaitForSeconds(m_reSpawnWaitTime);
-        SpawnNewCharacterInstance();
-        yield return null;
-    }
-    private void SpawnNewCharacterInstance()
-    {
-        Instantiate(m_mainCharacterPrefab, _lastCheckPoint, Quaternion.identity)
-        .TryGetComponent<MainCharacterController>(out MainCharacterController m_characterInstance);
-    }
-
 }
