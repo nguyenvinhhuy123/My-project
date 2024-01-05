d337ce76eaacdf8025cead0e28b985131ef61949
iSayU <103714465+nguyenvinhhuy123@users.noreply.github.com>
2024-01-05 10:13:52 +0700

    feat: add PlayerManager script

---
+++ Assets/Scenes/Script/Manager/PlayerManager/PlayerManager.cs
@@ -0,0 +1,51 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Utilities;
+
+public class PlayerManager : PersistenceSingleton<PlayerManager>
+{
+    private PlayerSpawner _spawner;
+    private PlayerLifeCounter _lifeCounter;
+    private MainCharacterController m_characterInstance;
+    public MainCharacterController CharacterInstance {get{ return m_characterInstance;}}
+    protected override void Awake()
+    {
+        base.Awake();
+        if (!gameObject.TryGetComponent<PlayerSpawner>(out _spawner))
+        {
+            _spawner = gameObject.AddComponent<PlayerSpawner>();
+        }
+        if (!gameObject.TryGetComponent<PlayerLifeCounter>(out _lifeCounter))
+        {
+            _lifeCounter = gameObject.AddComponent<PlayerLifeCounter>();
+        }
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void CharacterDestroy()
+    {
+        gameObject.BroadcastMessage("OnCharacterDestroy");
+    }
+    public void CharacterSpawn(MainCharacterController character)
+    {  
+        gameObject.BroadcastMessage("OnCharacterSpawn", character);
+    }
+    public void Checked(CheckPointController checkPoint)
+    {
+        gameObject.BroadcastMessage("OnChecked", checkPoint);
+    }
+    private void OnCharacterSpawn(MainCharacterController character)
+    {
+        m_characterInstance = character;
+    }
+}
