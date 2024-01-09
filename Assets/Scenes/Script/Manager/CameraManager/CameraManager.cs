333a99173f7497b9242c77a43e5e134696485f44
iSayU <103714465+nguyenvinhhuy123@users.noreply.github.com>
2024-01-09 11:31:24 +0700

    feat: camera follow mc whenever mc respawn

--- Assets/Scenes/Script/Manager/CameraManager/CameraManager.cs
+++ Assets/Scenes/Script/Manager/CameraManager/CameraManager.cs
@@ -2,18 +2,32 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Cinemachine;
+using UnityEngine.Events;
 
 public class CameraManager : MonoBehaviour
 {
     private MainCharacterController m_followTarget;
     private CinemachineVirtualCamera m_virtualCamera;
+    private UnityAction<MainCharacterController> m_onCharacterSpawnAction;
+    public UnityAction<MainCharacterController> OnCharacterSpawnAction {get {return m_onCharacterSpawnAction;}} 
     void Awake() {
         m_virtualCamera = GetComponent<CinemachineVirtualCamera>();
+    }
+    void OnEnable() {
+        m_onCharacterSpawnAction += SetFollowTarget;
+    }
+    void OnDisable()
+    {
+        m_onCharacterSpawnAction -= SetFollowTarget;  
     }
     // Start is called before the first frame update
     void Start()
     {
-        
+        PlayerManager.Instance.OnCharacterSpawnListenerRegister(OnCharacterSpawnAction);
+    }
+    void OnDestroy()
+    {
+        PlayerManager.Instance.OnCharacterSpawnListenerUnRegister(OnCharacterSpawnAction);
     }
     // Update is called once per frame
     void Update()
