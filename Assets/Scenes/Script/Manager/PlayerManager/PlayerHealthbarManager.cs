bc9523aa67b6e5fd885db340c297f0db171f5bda
iSayU <103714465+nguyenvinhhuy123@users.noreply.github.com>
2024-01-11 15:04:13 +0700

    feat: onHeal action and onHeal callback for playerHealthbar controller

--- Assets/Scenes/Script/Manager/PlayerManager/PlayerHealthbarManager.cs
+++ Assets/Scenes/Script/Manager/PlayerManager/PlayerHealthbarManager.cs
@@ -2,12 +2,14 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Events;
+using UnityEngine.UI;
 
 public class PlayerHealthbarManager : MonoBehaviour
 {
     private Damageable m_cachePlayerDamageable;
     //TODO: Health UI ref here
     private UnityAction<int, bool> m_onDamagedAction;
+    private UnityAction<int> m_onHealAction;
     // Start is called before the first frame update
     void Awake()
     {
@@ -20,10 +22,12 @@ public class PlayerHealthbarManager : MonoBehaviour
     void OnEnable()
     {
         m_onDamagedAction += OnDamage;
+        m_onHealAction += OnHeal;
     }
     void OnDisable()
     {
         m_onDamagedAction -= OnDamage;
+        m_onHealAction -= OnHeal;
     }
     // Update is called once per frame
     void Update()
@@ -39,10 +43,15 @@ public class PlayerHealthbarManager : MonoBehaviour
         }
 
     }
+    private void OnHeal(int currentHealth)
+    {
+
+    }
     private void OnCharacterSpawn(MainCharacterController character)
     {
         character.gameObject.TryGetComponent<Damageable>(out m_cachePlayerDamageable);
         m_cachePlayerDamageable.DamagedEventListenerRegister(m_onDamagedAction);
+        m_cachePlayerDamageable.HealEventListenerRegister(m_onHealAction);
     }
     private void OnCharacterDestroy()
     {
