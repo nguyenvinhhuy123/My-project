c044fb47624de7ffb057b005657ee4891dc13dde
iSayU <103714465+nguyenvinhhuy123@users.noreply.github.com>
2024-01-17 16:43:38 +0700

    feat: Mock Script to test healthbarUI behaviour

--- Assets/Scenes/Script/UIComponent/PlayerHealthbar/PlayerHealthBarUI.cs
+++ Assets/Scenes/Script/UIComponent/PlayerHealthbar/PlayerHealthBarUI.cs
@@ -10,11 +10,15 @@ public class PlayerHealthBarUI : Singleton<PlayerHealthBarUI>
     private Slider m_healthBarUI;
     private int m_cacheCurrentHealthValue;
     [SerializeField][Range(1,5)] private int m_sliderChangeLerpAmount = 2;
-    [SerializeField] private float m_sliderChangeLerpTime = 0.02f;
+    [SerializeField] private float m_sliderChangeLerpTimeSeconds = 0.1f;
     protected override void Awake()
     {
         base.Awake();
         m_healthBarUI = gameObject.GetComponent<Slider>();
+    }
+    void Start()
+    {
+        ResetHealthValue();
     }
     public void SetMaxHealthValue(int maxHealth)
     {
@@ -24,20 +28,29 @@ public class PlayerHealthBarUI : Singleton<PlayerHealthBarUI>
     }
     public void AdjustHealthValue(int newValue)
     {
-        throw new System.Exception("Not Implemented");
+        StartCoroutine(nameof(SliderHealthChangeLerpDown),newValue);
     }
     public void ResetHealthValue()
     {
         m_healthBarUI.value = m_healthBarUI.maxValue;
+        m_cacheCurrentHealthValue = (int)m_healthBarUI.maxValue;
     }
-    private IEnumerator SliderHealthChangeLerp(int targetValue, int startValue)
+    private IEnumerator SliderHealthChangeLerpDown(int targetValue)
     {
-        int currentValue = startValue;
-        if (currentValue == targetValue) yield break;
-
-        currentValue -= m_sliderChangeLerpAmount;
-        m_healthBarUI.value = currentValue;
-
-        yield return new WaitForSeconds(m_sliderChangeLerpTime);
+        int currentValue = m_cacheCurrentHealthValue;
+        while (currentValue > targetValue)
+        {
+            currentValue -= m_sliderChangeLerpAmount;
+            if (currentValue <= targetValue) currentValue = targetValue;
+            m_healthBarUI.value = currentValue;
+            Debug.Log(currentValue);
+            yield return new WaitForSeconds(m_sliderChangeLerpTimeSeconds);
+        }
+        if (currentValue <= targetValue) 
+        {
+            m_cacheCurrentHealthValue = targetValue;
+            yield break;
+        }
+        
     }
 }
