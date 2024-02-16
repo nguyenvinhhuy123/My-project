e2a7d440bcaf8fa7e81bdaa929fec72d73442276
iSayU <103714465+nguyenvinhhuy123@users.noreply.github.com>
2024-02-16 10:26:59 +0700

    feat: Pause Button implementation

---
+++ Assets/Scenes/Script/UIComponent/Buttons/PauseButton/PauseButton.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PauseButton : BaseButton
+{
+    public override void OnClick()
+    {
+        base.OnClick();
+        if (!GameManager.Instance.IsPause) GameManager.Instance.PauseGame();
+        else  GameManager.Instance.UnPauseGame();
+    }
+}
