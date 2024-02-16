6de1917741e49c9bfb61338c4570c766715a1cd7
iSayU <103714465+nguyenvinhhuy123@users.noreply.github.com>
2024-02-16 10:26:15 +0700

    feat: button interface

---
+++ Assets/Scenes/Script/UIComponent/Buttons/IButton.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public interface IButton
+{
+    #region Button Animation Name String
+    #endregion
+    void OnClick();
+    void OnHover();
+    void OnExitHover();
+}
